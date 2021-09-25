ae12db3ac16c9840dc936a57f9d2c32b711ae665
gottaboy <gottaboy@github.com>
2021-09-26 00:02:15 +0800

    添加网关zuul

---
+++ api-gateway/src/main/java/com/mysting/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.mysting;
+
+import com.mysting.filter.AccessFilter;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
+
+}
